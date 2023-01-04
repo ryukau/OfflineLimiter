f7fe96ad663736f943f0a3780f9d6a2eef5b4cda
Takamitsu Endo <ryukau@gmail.com>
2023-01-04 10:28:15 +0900

    Add -G option for cmake command

--- build.ps1
+++ build.ps1
@@ -4,6 +4,8 @@
 
 if (!(test-path build)) {
   cmake -S . -B build `
+    -G "Visual Studio 17 2022" `
+    -A x64 `
     -DCMAKE_TOOLCHAIN_FILE="/src/vcpkg/scripts/buildsystems/vcpkg.cmake" `
     -DCMAKE_BUILD_TYPE=Release
 }
